047a0825bf391657e875d323d77a730712cf3626
Mário Nogueira <71707492+MarioNogueira01@users.noreply.github.com>
2023-11-04 20:54:22 +0000

    Codigo final com temporizador

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
             Scanner sc = new Scanner(System.in);
+
             BestFirst s = new BestFirst();
             int number = sc.nextInt();
+            long startTimeMillis = System.currentTimeMillis();//temporizador
             Iterator<BestFirst.State> it = s.solve(new sequence(number, 0,0),
                     new sequence(number * 3, 0,Math.abs(number - number*3)));
+        long endTimeMillis = System.currentTimeMillis();//temporizador
+        long executionTimeMillis = endTimeMillis - startTimeMillis;//temporizador
+        long executionTimeSecs = executionTimeMillis/1000;//temporizador
            try {
 
 
@@ -18,7 +24,7 @@ public class Main {
                        System.out.println(i);
                        if (!it.hasNext()) {
                            System.out.println("\n" + i.getG());
-
+                           System.out.println("tempo de execucao : " + executionTimeSecs + " segundos e " + executionTimeMillis+" milisegundos");
                        }
                    }
                }
